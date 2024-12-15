9c0d4e93637f9af67ba02899d2cc06b0f8151230
Segera <your.segeramos89@gmail.com>
2024-12-15 10:39:29 +0300

    updated the services folder

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const API_URL = 'https://api.github.com/search/users';
+const USER_API_URL = 'https://api.github.com/users';
 
 export const searchUsers = async (query) => {
     const response = await axios.get(API_URL, {
@@ -9,4 +10,10 @@ export const searchUsers = async (query) => {
         }
     })
     return response.data.items;
+}
+
+// New function to fetch user data by username
+export const fetchUserData = async (username) => {
+    const response = await axios.get(`${USER_API_URL}/${username}`);
+    return response.data;
 }
